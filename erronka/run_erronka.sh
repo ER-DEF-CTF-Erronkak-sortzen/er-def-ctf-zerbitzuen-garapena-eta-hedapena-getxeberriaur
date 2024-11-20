9caa36b315ae6e68436bde77727d73ba00c1aeec
getxeberriaur <getxeberriaur@zubirimanteo.com>
2024-11-20 10:13:47 +0100

    z

--- erronka/run_erronka.sh
+++ erronka/run_erronka.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-#docker-compose -f "$SERVICES_PATH/erronka/docker-compose.yml" up -d --build
-docker-compose -f "/root/services/erronka/docker-compose.yml" up -d --build
+docker-compose -f "$SERVICES_PATH/erronka/docker-compose.yml" up -d --build
+#docker-compose -f "/root/services/erronka/docker-compose.yml" up -d --build
